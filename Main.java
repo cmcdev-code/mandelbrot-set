a192ad410498bceb25d28ae4a27800b0e3eb0fda
Collin McDevitt <cmcdev@vt.edu>
2023-08-11 23:27:38 -0400

    Adding readme

--- Main.java
+++ Main.java
@@ -40,23 +40,7 @@ public class Main {
 
         return real_range_array;
     }
-    // //function that will mimic the mandelbrot equation and will return the number of iterations 
-    // static int complex_function(double x0, double y0, int max_iterations){
-
-    //     int iterations=0;
-    //     double x=0.0,y=0.0;        
-    //     //https://en.wikipedia.org/wiki/Mandelbrot_set
-    //     for(;x*x +y*y <= 4.0 && iterations<max_iterations;iterations++ ){
-
-    //         double xtemp = x*x - y*y + x0;
-    //         y= 2*x*y + y0;
-    //         x=xtemp;
-    //     }
-    //     return iterations;
-    // }
-
-
- //function that will mimic the mandelbrot equation and will return the number of iterations 
+    //function that will mimic the mandelbrot equation and will return the number of iterations 
     static int complex_function(double x0, double y0, int max_iterations){
 
         int iterations=0;
@@ -65,11 +49,27 @@ public class Main {
         for(;x*x +y*y <= 4.0 && iterations<max_iterations;iterations++ ){
 
             double xtemp = x*x - y*y + x0;
-            y= 2*x* y + y0;
+            y= 2*x*y + y0;
             x=xtemp;
         }
         return iterations;
     }
+
+    //Function that will create a map of colors using the number of iterations the array of colors must divide number of iterations
+    static Map<Integer,Color> generate_color_map(int number_of_iterations, int[] array_of_colors){
+        Map<Integer, Color> colorMap = new HashMap<>();
+
+        int delta_colors= number_of_iterations/array_of_colors.length;
+        int index_for_switching_colors =0;
+
+        for(int i =1 ;i<=number_of_iterations ;i++){
+            colorMap.put(i-1,new Color(array_of_colors[index_for_switching_colors]));
+            index_for_switching_colors += ( i%delta_colors - 1>> 31) & 0x1;
+        }
+        colorMap.put(number_of_iterations,new Color(array_of_colors[index_for_switching_colors-1]));
+
+        return colorMap;
+    } 
 
 
 
@@ -77,6 +77,8 @@ public class Main {
   public static void main(String[] args) {
         int canvasWidth = 0;
         int canvasHeight = 0;
+
+        int number_of_threads=0;
         System.out.println("What do you want the width to be?");
         Scanner dim_input= new Scanner(System.in);
         
@@ -87,99 +89,18 @@ public class Main {
 
         canvasHeight=Integer.parseInt(dim_input.nextLine());
 
+        System.out.println("How many threads?");
+        number_of_threads=Integer.parseInt(dim_input.nextLine());
 
+
+        int number_of_iterations=500;
         dim_input.close();
-             // Create a dictionary to map iterations to colors
-        Map<Integer, Color> colorMap = new HashMap<>();
 
-        // Define colors for different iteration counts
-        colorMap.put(0, Color.BLACK);
+        //https://davidjohnstone.net/cubehelix-gradient-picker
+        int[] colors={0x000000,0x87cdae,0x6cb5c0,0x6e8fd0,0x8563c4,0x9b3f95,0xe962c4f,
+                      0x702911,0x372900,0xfad4c0};
 
-        // Define gradient colors
-        Color startColor = new Color(0x2a0e26);
-        Color endColor= new Color(0x4f97a2);
-     
-             // Add smooth gradient iteration-color mappings
-        for (int i = 1; i <= 20; i++) {
-            double t = (double) i / 100; // Normalized value between 0 and 1
-            Color interpolatedColor = interpolateColors(startColor, endColor, t);
-            colorMap.put(i, interpolatedColor);
-        }
-     
-        startColor=endColor;
-        endColor= new Color(0x6e69b1);
-   
-        
-
-        for(int i=21;i<=41;i++){
-            double t = (double) i / 100; // Normalized value between 0 and 1
-            Color interpolatedColor = interpolateColors(startColor, endColor, t);
-            colorMap.put(i, interpolatedColor);
-        }
-        
-
-        startColor=endColor;
-          endColor= new Color(0x8f4680);
-    
-
-
-        for(int i=42;i<=63;i++){
-            double t = (double) i / 100; // Normalized value between 0 and 1
-            Color interpolatedColor = interpolateColors(startColor, endColor, t);
-            colorMap.put(i, interpolatedColor);
-        }
-
-
-        startColor=endColor;
-        endColor= new Color(0x7f4036);
-
-
-        for(int i=64;i<=80;i++){
-            double t = (double) i / 100; // Normalized value between 0 and 1
-            Color interpolatedColor = interpolateColors(startColor, endColor, t);
-            colorMap.put(i, interpolatedColor);
-        }
-
-        startColor=endColor;
-       endColor = new Color(0x454811);
-
-        for(int i=81;i<=100;i++){
-            double t = (double) i / 100; // Normalized value between 0 and 1
-            Color interpolatedColor = interpolateColors(startColor, endColor, t);
-            colorMap.put(i, interpolatedColor);
-        }
-        
-        startColor=endColor;
-       endColor = new Color(0x14431d);
-              for(int i=101;i<=121;i++){
-            double t = (double) i / 100; // Normalized value between 0 and 1
-            Color interpolatedColor = interpolateColors(startColor, endColor, t);
-            colorMap.put(i, interpolatedColor);
-        }
-        
-
-        startColor=endColor;
-       endColor = new Color(0x092a2e);
-              for(int i=122;i<=143;i++){
-            double t = (double) i / 100; // Normalized value between 0 and 1
-            Color interpolatedColor = interpolateColors(startColor, endColor, t);
-            colorMap.put(i, interpolatedColor);
-        }
-         startColor=endColor;
-       endColor = new Color(0x0f0e1f);
-              for(int i=144;i<=165;i++){
-            double t = (double) i / 100; // Normalized value between 0 and 1
-            Color interpolatedColor = interpolateColors(startColor, endColor, t);
-            colorMap.put(i, interpolatedColor);
-        }
-
-           startColor=endColor;
-       endColor = new Color(0x000000);
-              for(int i=166;i<=200;i++){
-            double t = (double) i / 100; // Normalized value between 0 and 1
-            Color interpolatedColor = interpolateColors(startColor, endColor, t);
-            colorMap.put(i, interpolatedColor);
-        }
+        Map<Integer, Color> colorMap = generate_color_map(number_of_iterations,colors);
 
 
         JFrame frame = new JFrame("Pixel Canvas Example");
@@ -189,19 +110,26 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
-        
-
         double[] real_numbers_x= integer_to_real_range(0,canvasWidth,-2, 0.47);
         double[] real_numbers_y= integer_to_real_range(0, canvasHeight,-1.2,1.2);
+      
+        int[][] values=new int[canvasWidth][canvasHeight];
+        
+        int ind_thread_range_width= canvasWidth/number_of_threads;
+        int ind_thread_range_height = canvasHeight/number_of_threads;
+
+        
+
+
 
         for(int i=0; i<canvasWidth;i++){
             for(int j=0;j<canvasHeight;j++){
-                pixelCanvas.drawPixel(i,j,colorMap.get(complex_function(real_numbers_x[i],real_numbers_y[j],200 )));
+                pixelCanvas.drawPixel(i,j,colorMap.get(complex_function(real_numbers_x[i],real_numbers_y[j],number_of_iterations )));
             }
         }
         pixelCanvas.repaint();
 
-    pixelCanvas.saveCanvasAsImage("canvas2.png", "PNG");
+    pixelCanvas.saveCanvasAsImage("canvas321322.png", "PNG");
 
   }
 }
---
+++ ReadMe.md
@@ -0,0 +1,45 @@
+# The Mandelbrot Fractal 
+
+![](https://github.com/cmcdev-code/../../../media/fractal100.gif)
+
+This code is written in java and will just produce an image of the mandelbrot fractal.
+
+
+The function bellow can be modified to produce different fractals on the complex plane with ease. 
+
+
+```java
+
+  static int complex_function(double x0, double y0, int max_iterations){
+
+        int iterations=0;
+        double x=0.0,y=0.0;        
+        for(;x*x +y*y <= 4.0 && iterations<max_iterations;iterations++ ){
+
+            double xtemp = x*x - y*y + x0;
+            y= 2*x*y + y0;
+            x=xtemp;
+        }
+        return iterations;
+    }
+
+```
+
+This other function bellow uses branchless programming to know when to increment to the next color. In other languages boolean values either are a integer 0 or 1 while in java they are only denoted "true/false". So the line instead gets the index mod delta color and then subtracts 1 from it. Because it is a signed integer the 31st bit will only be a 1 if it is negative and otherwise will be just a 0, so thats what I do instead.
+
+```java
+    static Map<Integer,Color> generate_color_map(int number_of_iterations, int[] array_of_colors){
+        Map<Integer, Color> colorMap = new HashMap<>();
+
+        int delta_colors= number_of_iterations/array_of_colors.length;
+        int index_for_switching_colors =0;
+
+        for(int i =1 ;i<=number_of_iterations ;i++){
+            colorMap.put(i-1,new Color(array_of_colors[index_for_switching_colors]));
+            index_for_switching_colors += ( i%delta_colors - 1>> 31) & 0x1;
+        }
+        colorMap.put(number_of_iterations,new Color(array_of_colors[index_for_switching_colors-1]));
+        return colorMap;
+
+    } 
+```
